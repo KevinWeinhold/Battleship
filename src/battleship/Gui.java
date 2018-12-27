1a17ee84ba95cd2390b4cc36d19c6aa898919eb6
Kevin <kweinhold22@gmail.com>
2018-12-26 20:11:44 -0700

    basic swing frame

--- src/battleship/Gui.java
+++ src/battleship/Gui.java
@@ -1,10 +1,23 @@
 package battleship;
 
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
 public class Gui {
 
 	public static void main(String[] args) {
-		System.out.println("Herro");
-
+		SwingUtilities.invokeLater(new Runnable() {
+			public void run() {
+				
+				JFrame frame = new JFrame("Hello World Swing!");
+				frame.setSize(500, 400);
+				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+				frame.setVisible(true);
+			}
+		});
+		
+	
+		
 	}
 
 }
