62255ada3eb0bb0cbc0373c497944183d29f87bb
Kevin <kweinhold22@gmail.com>
2018-12-27 15:06:07 -0700

    Fixing conflicts

---
+++ src/battleship/App.java
@@ -0,0 +1,23 @@
+package battleship;
+
+import java.util.Scanner;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
+
+public class App {
+	
+	private static Gui myBoard;
+	private static Game myGameBoard;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	 
+		myBoard = new Gui();
+		//myBoard.start();
+		
+		//myGameBoard = new Game();
+		
+		}
+		
+
+	}
